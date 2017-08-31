12b3e8fac5f530d072dec535a0b52c4f64953b9f
Thiago Guardado Beltran <beltran.thiago@gmail.com>
2017-08-31 14:11:46 -0300

    Setup Inicial

---
+++ Assets/Scripts/Brick_Center.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick_Center : MonoBehaviour {
+
+	public int brickID;
+	public float brickDistanceFromCenter;
+	public GameObject brick;
+
+
+	void Awake(){
+
+
+		brick.transform.localPosition = new Vector3 (0, 0, brickDistanceFromCenter);
+
+	}
+
+
+}
---
+++ Assets/Scripts/PathCenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathCenter : MonoBehaviour {
+
+	public int pathID;
+	public float rotationSpeed;
+	public float distanceFromCenter;
+	public int numberOfBricks;
+	public GameObject brick;
+
+
+	// Use this for initialization
+	void Awake() {
+
+
+		for (int i = 0; i < numberOfBricks; i++) {
+
+			Vector3 instPosition = transform.position + new Vector3(0,0,distanceFromCenter);
+			GameObject go = Instantiate (brick, instPosition, Quaternion.identity, transform);
+			go.transform.RotateAround(Vector3.zero,Vector3.up, 360 / (float)numberOfBricks * i);
+
+		}
+
+	}
+
+
+	void Update(){
+	
+		transform.Rotate (Vector3.up, rotationSpeed * Time.deltaTime);
+	
+	}
+	
+
+}
